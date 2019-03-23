a4388f34692b84e0179e941c41bbcab92170673f
jainszhang <jainszhang@163.com>
2019-03-23 17:06:39 +0800

    添加了linux的客户端1.5

--- linux/client.cpp
+++ linux/client.cpp
@@ -1,141 +1,12 @@
-#define WIN32_LEAN_AND_MEAN
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-
-#ifdef _WIN32
-	#include<WinSock2.h>
-	#include<Windows.h>
-#else
-	#include<unistd.h>//uni std
-	#include<arpa/inet.h>
-	#include<string.h>
-	#define SOCKET int
-	#define INVALID_SOCKET (SOCKET)(~0)
-	#define SOCKET_ERROR	(-1)
-#endif
-
-#include<stdio.h>
+ï»¿
 #include<thread>
-#pragma comment(lib,"ws2_32.lib")//¾²Ì¬Á´½Ó¿â
-//ctrl + k ctrl + c×¢ÊÍ´úÂë£¬ctrl+uÈ¡Ïû×¢ÊÍ´úÂë
+#include"EasyTcpClient.hpp"
+#pragma comment(lib,"ws2_32.lib")//?????
+//ctrl + k ctrl + c?????ctrl+u??????
 
 
 
-enum CMD
-{
-	CMD_LOGIN,
-	CMD_LOGIN_RESULT,
-	CMD_LOGINOUT,
-	CMD_LOGINOUT_RESULT,
-	CMD_NEW_USER_JOIN,
-	CMD_ERROR
-};
-///ÏûÏ¢Í·
-struct DataHeader
-{
-	short dataLength;//±íÊ¾Êý¾Ý³¤¶È
-	short cmd;//ÃüÁî
-};
-
-//DataPackage
-struct Login:public DataHeader
-{
-	Login()
-	{
-		dataLength = sizeof(Login);
-		cmd = CMD_LOGIN;
-	}
-	char userName[32];
-	char PassWord[32];
-};
-
-struct LoginResult:public DataHeader
-{
-	LoginResult()
-	{
-		dataLength = sizeof(LoginResult);
-		cmd = CMD_LOGIN_RESULT;
-	}
-	int result;
-
-};
-
-struct LoginOut:public DataHeader
-{
-	LoginOut()
-	{
-		dataLength = sizeof(LoginOut);
-		cmd = CMD_LOGINOUT;
-	}
-	char userName[32];
-};
-struct LoginOutResult:public DataHeader
-{
-	LoginOutResult()
-	{
-		dataLength = sizeof(LoginOutResult);
-		cmd = CMD_LOGINOUT_RESULT;
-	}
-	int result;
-
-};
-
-struct NewUserJoin :public DataHeader
-{
-	NewUserJoin()
-	{
-		dataLength = sizeof(NewUserJoin);
-		cmd = CMD_NEW_USER_JOIN;
-		sock = 0;
-	}
-	int sock;
-
-};
-
-int processor(SOCKET _cSock)
-{
-	//»º³åÇø
-	char szRecv[1024] = {};
-
-	//5.½ÓÊÕ¿Í»§¶ËµÄÊý¾Ýc
-	int nLen = recv(_cSock, (char *)szRecv, sizeof(DataHeader), 0);
-	DataHeader* header = (DataHeader*)szRecv;
-	if (nLen <= 0)
-	{
-		printf("disconnected with server, task end\n");
-		return -1;
-	}
-
-
-	switch (header->cmd)
-	{
-	case CMD_LOGIN_RESULT:
-	{
-		recv(_cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);//Ö®Ç°ÒÑ¾­°Ñheader¶ÁÈ¡³öÀ´ÁË£¬²»ÄÜÔÙ´ÓÍ·¶ÁÈ¡ÁË
-		printf("login result\n");
-		LoginResult* login_Result = (LoginResult*)szRecv;
-		printf("server returned data£ºCMD_LOGIN_RESULT£¬data length%d\n", login_Result->dataLength);
-	}
-	break;
-	case CMD_LOGINOUT_RESULT:
-	{
-		recv(_cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);//Ö®Ç°ÒÑ¾­°Ñheader¶ÁÈ¡³öÀ´ÁË£¬²»ÄÜÔÙ´ÓÍ·¶ÁÈ¡ÁË
-		LoginOutResult* loginout_Result = (LoginOutResult*)szRecv;
-		printf("server returned data£ºCMD_LOGINOUT_RESULT£¬data length%d\n", loginout_Result->dataLength);
-	}
-	break;
-
-	case CMD_NEW_USER_JOIN:
-	{
-		recv(_cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);//Ö®Ç°ÒÑ¾­°Ñheader¶ÁÈ¡³öÀ´ÁË£¬²»ÄÜÔÙ´ÓÍ·¶ÁÈ¡ÁË
-		NewUserJoin* userJoin = (NewUserJoin*)szRecv;
-		printf("server returned data£ºNEW_USER_JOIN£¬data length%d\n", userJoin->dataLength);
-	}
-	break;
-	}
-}
-
-bool g_bRun = true;
-void cmdThread(SOCKET _sock)
+void cmdThread(EasyTcpClient *client)
 {
 	while (true)
 	{
@@ -143,8 +14,8 @@ void cmdThread(SOCKET _sock)
 		scanf("%s", cmdBuf);
 		if (0 == strcmp(cmdBuf, "exit"))
 		{
-			g_bRun = false;
-			printf("ÍË³ö\n");
+			client->Close();
+			printf("??\n");
 			break;
 		}
 		else if (0 == strcmp(cmdBuf, "login"))
@@ -152,13 +23,15 @@ void cmdThread(SOCKET _sock)
 			Login login;
 			strcpy(login.userName, "zj");
 			strcpy(login.PassWord, "jainss");
-			send(_sock, (const char *)&login, sizeof(Login), 0);
+			client->SendData(&login);
+
+
 		}
 		else if (0 == strcmp(cmdBuf, "loginout"))
 		{
 			LoginOut loginout;
 			strcpy(loginout.userName, "zj");
-			send(_sock, (const char *)&loginout, sizeof(LoginOut), 0);
+			client->SendData(&loginout);
 		}
 		else
 		{
@@ -171,83 +44,28 @@ void cmdThread(SOCKET _sock)
 
 int main()
 {
-	//Æô¶¯windows socket2.x»·¾³
-#ifdef WIN32
-	WORD ver = MAKEWORD(2, 2);
-	WSADATA dat;
-	WSAStartup(ver, &dat);
-#endif
-	////
-
-	SOCKET _sock = socket(AF_INET,SOCK_STREAM,0);
-	
-	if (INVALID_SOCKET == _sock)
+	EasyTcpClient client;
+	int ret = client.InitSocket();
+	if (!ret)
 	{
-		printf("ERROR,connect failed...\n");
-	}
-	else
-	{
-		printf("connect success...\n");
-	}
-	//2.Á¬½Ó·þÎñÆ÷
-	sockaddr_in _sin = {};
-	_sin.sin_family = AF_INET;
-	_sin.sin_port = htons(4567);
-	_sin.sin_addr.s_addr = inet_addr("127.0.0.1");//win:192.168.236.128
-	if(SOCKET_ERROR == connect(_sock, (sockaddr*)&_sin, sizeof(sockaddr_in)))
-	{
-		printf("ERROR, connect socket failed...\n");
-	}
-	else
-	{
-		printf("connect socket success...\n");
+		client.Connect("192.168.236.128", 4567);
 	}
 
-	///´´½¨Ïß³Ìº¯Êý
-	std::thread t1(cmdThread,_sock);
-	t1.detach();//ºÍÖ÷Ïß³Ì·ÖÀë
+	//??UI??
+	std::thread t1(cmdThread,&client);
+	t1.detach();//??????
 
-
-	char cmdBuf[128] = {};
-	while (g_bRun)
+	while (client.isRun())
 	{
-		fd_set fdReads;
-		FD_ZERO(&fdReads);
-		FD_SET(_sock, &fdReads);
-		timeval t = { 1,0 };
-		//¼ì²â¸ÃsockÉÏÓÐÃ»ÓÐIO²Ù×÷£¬Èç¹ûÓÐ±£Áô¸Ãsock
-		int ret = select(_sock+1, &fdReads, 0, 0, &t);
-		if (ret < 0)
-		{
-			printf("select task over\n");
+		if (!client.onRun())
 			break;
-		}
-
-		if (FD_ISSET(_sock, &fdReads))
-		{
-			FD_CLR(_sock, &fdReads);
-			if (-1 == processor(_sock))
-			{
-				printf("seletc tase over2\n");
-				break;
-			}
-		}
-		//Ïß³Ìthread
-		
-
-//		printf("¿Í»§¶Ë¿ÕÏÐ£¬´¦ÀíÆäËûÈÎÎñ\n");
-
-
+		/*printf("client idle?processing other tasks...\n");
+		Sleep(1000);*/
 	}
 
-	
-
-	close(_sock);
-
+	client.Close();
 	printf("exited\n");
 	getchar();
-#ifdef WIN32
-	WSACleanup();
-#endif
+
 	return 0;
 }
