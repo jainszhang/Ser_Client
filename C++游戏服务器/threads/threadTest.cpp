c8e14133fc1fbefd34cab2b1e8a4b5fb67679feb
jainszhang <jainszhang@163.com>
2019-04-14 21:07:54 +0800

    添加线程交互1和线程交互2（使用互斥锁开启多个工作线程）

--- C++游戏服务器/threads/threadTest.cpp
+++ C++游戏服务器/threads/threadTest.cpp
@@ -8,6 +8,8 @@
 
 #include "threadTest.hpp"
 
+///创建线程的几种方式
+///多线程计算数字和
 template <typename Iter,typename Fun>
 double visitRange(thread::id id,Iter iterBegin,Iter iterEnd,Fun func)
 {
@@ -27,13 +29,10 @@ double visitRange(thread::id id,Iter iterBegin,Iter iterEnd,Fun func)
     }
     return v;
 }
-
-
 void printAll(int a,int b,int c)
 {
     cout<<a<<" "<<b<<" "<<c<<endl;
 }
-///创建线程的几种方式
 void testThreadInit()
 {
     int a=3,b=4,c=5;
@@ -46,19 +45,22 @@ void testThreadInit()
     thread t3(printAll,ref(a),ref(b),ref(c));//引用传递
     t3.join();
 }
-
 int work(int a)
 {
     return a+a;
 }
-
 template <class Iter>
 void realWork(Counter &c,double &totalValue,Iter b,Iter e)
 {
     for(;b!=e;++b)
     {
         totalValue+=work(*b);
+        ///临界区的操作，互斥量的使用
+//        c.lockMutex();
+        //此处有两个原子操作，但是两个执行之间还是有空的时间，因此还会出错
         c.addCount();
+//        c.addResource(1);
+//        c.unlockMutex();
     }
 }
 void add(int a,int b,int &c)
@@ -68,6 +70,13 @@ void add(int a,int b,int &c)
 bool printStep(Counter &c,int maxCount)
 {
     auto C = c.count();
+    ///临界区
+//    c.lockMutex();
+//    auto ave = c.aveResource();
+//    if(ave!=1)
+//        cout<<"has bad thing happend"<<endl;
+//    c.unlockMutex();
+
     if(C==maxCount)
     {
         cout<<"ok,finished"<<endl;
@@ -119,7 +128,12 @@ void interface()
     printCount.join();
 }
 
-//thread 的两种死法
+
+
+
+
+
+///thread 的两种死法
 void joinWorker()
 {
     cout<<"hello world"<<endl;
@@ -127,12 +141,119 @@ void joinWorker()
 void threadDied()
 {
     thread j(joinWorker);
-    //    thread j1(ff);
-    //    j1.join();//阻塞模式，子线程执行完毕后，主线程才继续执行.thread::join()会清理子线程相关的内存空间，此后thread object将不再和这个子线程相关了，即thread object不再joinable了，所以join对于一个子线程来说只可以被调用一次
-    //    j.join();
+//    thread j1(ff);
+//    j1.join();//阻塞模式，子线程执行完毕后，主线程才继续执行.thread::join()会清理子线程相关的内存空间，此后thread object将不再和这个子线程相关了，即thread object不再joinable了，所以join对于一个子线程来说只可以被调用一次
+//    j.join();
     cout<<j.joinable()<<endl;
     j.detach();//非阻塞模式，子线程分离，子和主两者同时进行
     cout<<"main"<<endl;
     this_thread::sleep_for(std::chrono::seconds(7));
+    cout<<"over"<<endl;
+}
+
+
+
+
+/////线程之间的交互1
+mutex mmutex;
+atomic<bool> ready{false};
+void worker(int i)
+{
+    while (!ready) {
+        //do something
+//        std::this_thread::yield();//仅仅是释放cpu，但是此处多个线程，释放后其他线程立马抢走cpu资源
+        std::this_thread::sleep_for(std::chrono::seconds(1));//每个线程到此处sleep1秒，然后再检查ready的值
+    }
+    std::lock_guard<std::mutex>lock(mmutex);//构造时上锁，析构时解锁，loc对象是局部对象，因此该函数结束时调用析构函数解锁
+    cout<<"hello world "<<i<<endl;
+}
+void threadConnect1()
+{
+    const auto threadCount = 4;
+    vector<thread>pool;
+    for (int i=0; i<threadCount; i++) {
+        pool.emplace_back(thread(worker,i));
+    }
+    std::this_thread::sleep_for(std::chrono::minutes(1));//停留1分钟
+    ready = true;
+    for(auto &v:pool)
+    {
+        if(v.joinable())
+        {
+            v.join();
+        }
+    }
+    cout<<"bye bye"<<endl;
+}
+
+
+
+
+
+///线程交互2
+class Message{
+public:
+    const std::string&data() const {return m_data;}
+    Message(std::string d):m_data(std::move(d)){}
+private:
+    std::string m_data;
+};
+std::mutex mymutex2;//表示所有对globalList的操作都是互斥的
+std::atomic<bool> ready2{false};
+std::atomic<bool> quit2{false};
+std::list<Message> globalList;
+
+void worker2()
+{
+    while (!ready2) {
+
+    }
+    while (!quit2) {
+        std::lock_guard<std::mutex> lock(mymutex2);
+        if(globalList.empty())
+            continue;
+        auto iter = globalList.begin();
+        //do real work and erase it
+        globalList.erase(iter);
+    }
+}
+void threadConnect2()
+{
+    const auto threadCount = 4;
+    //提前加入信息
+    for (int i=0; i<10000; i++) {
+        globalList.push_back("this is a test"+std::to_string(i));//i转为字符串
+    }
+    
+    vector<thread>pool;//线程池
+    ///开启4个工作线程
+    for (int i=0; i<threadCount; i++) {
+        pool.emplace_back(thread(worker2));
+    }
+    
+    ready2 = true;//表示准备好了，工作线程开始互斥的干活了
+    
+    //线程开始干活时，又加入了新的信息，且加入时也是互斥操作，与后台的线程不会冲突
+    for (int i=0; i<10000; i++) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        std::lock_guard<std::mutex>lock(mymutex2);
+        globalList.push_back(std::string("second"));//i转为字符串
+    }
+    
+    //主线程开启，主线程检查globalList是否为空，如果为空则停止所有线程
+    while (true) {
+        std::lock_guard<std::mutex>lock(mymutex2);
+        if(globalList.empty())
+        {
+            quit2 = true;//退出标志
+            break;
+        }
+    }
+
+    //释放所有线程
+    for (auto &v:pool){
+        if(v.joinable())
+            v.join();
+    }
     cout<<"over"<<endl;
 }
--- C++游戏服务器/threads/threadTest.hpp
+++ C++游戏服务器/threads/threadTest.hpp
@@ -20,20 +20,38 @@ using namespace std;
 #include <cmath>
 #include <future>
 #include <vector>
+#include <mutex>
+#include <list>
 //定义类
 class Counter{
 public:
     void addCount(){m_count++;}
+    
+    void addResource(int r){m_totalResource++;}
+    
     int count()const{return m_count;}
-    Counter():m_count(0){}
+    int aveResource(){
+        if(m_count==0)
+            return 1;
+        else
+            return m_totalResource / m_count;
+        }
+    Counter():m_count(0),m_totalResource(0){}
+    
+    void lockMutex(){m_mutex.lock();}
+    void unlockMutex(){m_mutex.unlock();}
 private:
-    std::atomic<int> m_count;//定义原子操作
-//    atomic_uint32_t m_count;//也可以使用这种内部方式
+    atomic<int> m_count;//定义原子操作
+    //auto<int>类型，跟int类型基本一样，可以操作很多++ -- + - * /等都支持
+//    atomic_int m_count;//也可以使用这种内部方式
+    atomic_int m_totalResource;
+    mutex m_mutex;//互斥量，互斥访问
+    
 };
 
-
-
 //函数声明
-void testThreadInit();
-void interface();
+//void testThreadInit();
+//void interface();
+//void threadDied();//thread的两种死法
+void threadConnect2();//线程间交互2
 #endif /* threadTest_hpp */
